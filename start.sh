a7a3096d091fbabf00b1a6f54c75be201d80f0f4
mickeydarrenlau <darrenwjlau@gmail.com>
2023-03-02 06:15:55 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 apt update -y && apt install -y curl
 curl -fsSL https://tailscale.com/install.sh | sh
-tailscaled -tun userspace-networking &
+tailscaled -tun userspace-networking -statedir=/app/tailscaled &
 tailscale up --accept-routes --ssh
 bash /app.sh
